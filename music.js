8f4f8e981a50d994d5c6259d42cef7170e7e6e47
Ungkimyou <39607284+Ungkimyou@users.noreply.github.com>
2018-10-06 01:53:23 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 const { Client, Util } = require('discord.js');
-const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config.json');
 const Youtube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
